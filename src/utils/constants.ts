81de0427d942025ce302324c9ff50c28a6c05d61
sanketparsewar <sanket.parsewar97@gmail.com>
2026-04-07 10:59:45 +0530

    update website with new domain

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,4 +4,4 @@ const url = import.meta.env.VITE_APP_URL;
 export const brandName = new URL(url).hostname.split(".")[0];
 
 // Brand name with camel caseing
-export const BRANDNAME = "Scan2Queue";
+export const BRANDNAME = "QueueBuddy";
